2dc1fe99b4dcfa4d3673004b0440b14523fa421c
olayinkasanusi <sanusiolayinka7@gmail.com>
2026-02-12 20:03:01 +0100

    //

--- contracts/Fundme.sol
+++ contracts/Fundme.sol
@@ -2,39 +2,43 @@
 
 pragma solidity ^0.8.18;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
 
 contract FundMe{
+    using PriceConverter for uint256;
 
-uint256 public minimumUsd = 5;
+uint256 public minimumUsd = 5e18;
+mapping(address=> uint256) public addressToAmountFunded;
 
+address[] public funders;
     function fund() public payable {
-        //Allow users to send $
-        // Have a minimum amount of $ sent
-        // 1. How do we send ETH to this contract?
+        require( msg.value.getConversionRate() >= minimumUsd, "didn't send enough ETH");
+        funders.push(msg.sender);
 
-        require(msg.value >= minimumUsd, "didn't send enough ETH");
+        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
+    
     }
 
     function withdraw() public
     {
     }
 
-    function getPrice() public view returns(uint256){
-    AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-    (, int256 price,,,)=priceFeed.latestRoundData();
-    // Price of ETH in terms of USD
+//     function getPrice() public view returns(uint256){
+//     AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
 
-    return uint256(price * 1e10);
-    }
-function getConversionRate(uint256 ethAmount) public view returns(uint256) {
-    uint256 ethPrice = getPrice();
-    uint256 ethAmountInUsd = (ethAmount *ethPrice)/ ( 1e18);
-    return ethAmountInUsd;
-    }
+//     (, int256 price,,,)=priceFeed.latestRoundData();
+//     // Price of ETH in terms of USD
 
-    function getVersion() public view returns(uint256){
-        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+//     return uint256(price * 1e10);
+//     }
+// function getConversionRate(uint256 ethAmount) public view returns(uint256) {
+//     uint256 ethPrice = getPrice();
+//     uint256 ethAmountInUsd = (ethAmount *ethPrice)/ ( 1e18);
+//     return ethAmountInUsd;
+//     }
+
+//     function getVersion() public view returns(uint256){
+//         return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
    
-    }
+//     }
 }
