aa9773e0ca6306027c8201657ebcd6b50863b1aa
olayinkasanusi <sanusiolayinka7@gmail.com>
2026-02-12 14:54:03 +0100

    //

--- contracts/Fundme.sol
+++ contracts/Fundme.sol
@@ -1,3 +1,23 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.18;
+
+contract FundMe{
+
+uint256 public minimumUsd = 5;
+
+    function fund() public payable {
+        //Allow users to send $
+        // Have a minimum amount of $ sent
+        // 1. How do we send ETH to this contract?
+
+   
+
+        require(msg.value >= minimumUsd, "didn't send enough ETH");
+    }
+
+function withdraw() public
+{
+
+}
+}
