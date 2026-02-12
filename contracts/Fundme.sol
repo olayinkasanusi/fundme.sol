93e14e976d51b207a79edf3f3716ec0023437cf3
olayinkasanusi <sanusiolayinka7@gmail.com>
2026-02-12 16:21:05 +0100

    //

--- contracts/Fundme.sol
+++ contracts/Fundme.sol
@@ -2,6 +2,24 @@
 
 pragma solidity ^0.8.18;
 
+// solhint-disable-next-line interface-starts-with-i
+interface AggregatorV3Interface {
+  function decimals() external view returns (uint8);
+
+  function description() external view returns (string memory);
+
+  function version() external view returns (uint256);
+
+  function getRoundData(
+    uint80 _roundId
+  ) external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+
+  function latestRoundData()
+    external
+    view
+    returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+}
+
 contract FundMe{
 
 uint256 public minimumUsd = 5;
@@ -11,13 +29,23 @@ uint256 public minimumUsd = 5;
         // Have a minimum amount of $ sent
         // 1. How do we send ETH to this contract?
 
-   
-
         require(msg.value >= minimumUsd, "didn't send enough ETH");
     }
 
-function withdraw() public
-{
+    function withdraw() public
+    {
+    }
 
+    function getPrice() public{
+    // ADDRESS: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+    // ABI
 }
+function getConversionRate() public{
+
+    }
+
+    function getVersion() public view returns(uint256){
+        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+   
+    }
 }
