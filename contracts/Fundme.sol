ac8a1934450e3eaf61d1f78dd5690fd2ea6074b1
olayinkasanusi <sanusiolayinka7@gmail.com>
2026-02-13 16:44:19 +0100

    //

--- contracts/Fundme.sol
+++ contracts/Fundme.sol
@@ -11,34 +11,42 @@ uint256 public minimumUsd = 5e18;
 mapping(address=> uint256) public addressToAmountFunded;
 
 address[] public funders;
+
+    address public owner;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
     function fund() public payable {
         require( msg.value.getConversionRate() >= minimumUsd, "didn't send enough ETH");
         funders.push(msg.sender);
 
-        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
+        addressToAmountFunded[msg.sender] += msg.value;
     
     }
 
-    function withdraw() public
+    function withdraw() public onlyOwner
     {
+        
+        for(uint256 funderIndex=0; funderIndex <funders.length; funderIndex++){
+           address funder = funders[funderIndex];
+           addressToAmountFunded[funder] = 0;
+        }
+
+        funders = new address[](0);
+
+        // payable(msg.sender).transfer(address(this).balance);
+
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "failed to send ETH");
+
+        (bool callSuccess, )=payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
     }
 
-//     function getPrice() public view returns(uint256){
-//     AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-
-//     (, int256 price,,,)=priceFeed.latestRoundData();
-//     // Price of ETH in terms of USD
-
-//     return uint256(price * 1e10);
-//     }
-// function getConversionRate(uint256 ethAmount) public view returns(uint256) {
-//     uint256 ethPrice = getPrice();
-//     uint256 ethAmountInUsd = (ethAmount *ethPrice)/ ( 1e18);
-//     return ethAmountInUsd;
-//     }
-
-//     function getVersion() public view returns(uint256){
-//         return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
-   
-//     }
+    modifier onlyOwner(){
+        require(msg.sender == owner, 'Must be owner!');
+        _;
+    }
 }
--- contracts/SafeMathTester.sol
+++ contracts/SafeMathTester.sol
@@ -1,2 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
+
+contract SafeMathTester{
+    uint public bigNumber = 255;
+}
